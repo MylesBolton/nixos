ac80b4a1e9da9b9339c80c099811349c892071e2
MylesBolton <git@mylesbolton.com>
2025-02-24 16:55:12 +0000

    maybe fix running out of space on installation from usb iso

--- systems/x86_64-install-iso/minimal/default.nix
+++ systems/x86_64-install-iso/minimal/default.nix
@@ -23,5 +23,11 @@
     initialPassword = "1";
   };
 
+  fileSystems."/nix/.rw-store" = {
+    fsType = "tmpfs";
+    options = [ "mode=0755" "nosuid" "nodev" "relatime" "size=32G" ];
+    neededForBoot = true;
+  };
+
   system.stateVersion = "24.05";
 }
