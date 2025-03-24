eb44226ee7fd661abf66a63f5f8944fbce012cc0
MylesBolton <github@mylesbolton.com>
2025-03-24 11:47:24 +0000

    iso fix

--- systems/x86_64-install-iso/minimal/default.nix
+++ systems/x86_64-install-iso/minimal/default.nix
@@ -1,26 +1,23 @@
 { lib, namespace, ... }:
 {
-  system = {
-    custom = {
+  custom = {
+    user = {
+      name = "nixos";
+      initialPassword = "1337";
+    };
+    system = {
       nix.enable = true;
       networking.enable = true;
       locale.enable = true;
       boot.enable = true;
     };
-  };
-
-  services = {
-    openssh.enable = true;
-    custom = {
+    services = {
+      openssh.enable = true;
       tailscale.enable = true;
     };
   };
-  styles.stylix.enable = true;
 
-  user = {
-    name = "nixos";
-    initialPassword = "1337";
-  };
+  styles.stylix.enable = true;
 
   fileSystems."/nix/.rw-store" = {
     fsType = "tmpfs";
