e4daa610a0ee45730cdaa46583436fd3240eef3f
MylesBolton <github@mylesbolton.com>
2025-04-17 01:57:30 +0100

    formatter

--- modules/home/apps/vscode/default.nix
+++ modules/home/apps/vscode/default.nix
@@ -56,6 +56,9 @@ in
             "serverPath" = "nil";
           };
           "projectManager.git.baseFolders" = [ "/home/user/git" ];
+          "[nix]" = {
+            "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
+          };
         };
         keybindings = [
           {
