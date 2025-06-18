24aa252743c4e9feac1d42ad468960c66fc67f4a
MylesBolton <git@MylesBolton.com>
2025-06-18 18:38:26 +0100

    gnome dconf and remove kde

--- modules/home/roles/desktop/default.nix
+++ modules/home/roles/desktop/default.nix
@@ -29,5 +29,53 @@ in
       keepassxc
       #rustdesk
     ];
+    dconf.settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [
+          "user-theme@gnome-shell-extensions.gcampax.github.com"
+          "blur-my-shell@aunetx"
+          "caffeine@patapon.info"
+          "dash-to-dock@micxgx.gmail.com"
+          "gsconnect@andyholmes.github.io"
+          "sound-percentage@subashghimire.info.np"
+          "syncthing@gnome.2nv2u.com"
+          "tactile@lundal.io"
+        ];
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        always-center-icons = true;
+        animation-time = 0.10000000000000002;
+        apply-custom-theme = false;
+        background-color = "rgb(21,23,35)";
+        background-opacity = 1.0;
+        click-action = "focus-minimize-or-previews";
+        custom-background-color = true;
+        custom-theme-shrink = true;
+        dash-max-icon-size = 36;
+        dock-fixed = false;
+        dock-position = "BOTTOM";
+        extend-height = false;
+        height-fraction = 0.9;
+        hide-delay = 0.2;
+        hide-tooltip = false;
+        icon-size-fixed = false;
+        intellihide-mode = "FOCUS_APPLICATION_WINDOWS";
+        isolate-monitors = true;
+        isolate-workspaces = true;
+        middle-click-action = "launch";
+        pressure-threshold = 50.0;
+        running-indicator-style = "METRO";
+        scroll-action = "cycle-windows";
+        scroll-to-focused-application = true;
+        shift-click-action = "minimize";
+        shift-middle-click-action = "quit";
+        show-apps-always-in-the-edge = false;
+        show-apps-at-top = true;
+        show-favorites = true;
+        show-mounts-network = true;
+        show-trash = true;
+        transparency-mode = "DEFAULT";
+      };
+    };
   };
 }
