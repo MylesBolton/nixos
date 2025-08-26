18c4107133568b8e3003d4943a33ee1b6d4c1a42
MylesBolton <git@MylesBolton.com>
2025-08-26 23:09:59 +0100

    fixed batteyr and removed ventoy bc its insecure

--- modules/nixos/roles/desktop/default.nix
+++ modules/nixos/roles/desktop/default.nix
@@ -44,7 +44,7 @@ in
       wl-clipboard
       rustdesk-flutter
       ghostty
-      ventoy-full-gtk
+      #ventoy-full-gtk
     ];
 
     services = {
--- modules/nixos/roles/laptop/default.nix
+++ modules/nixos/roles/laptop/default.nix
@@ -17,8 +17,10 @@ in
   };
 
   config = mkIf cfg.enable {
-    system = {
-      battery.enable = true;
+    custom = {
+      system = {
+        battery.enable = true;
+      };
     };
   };
 }
