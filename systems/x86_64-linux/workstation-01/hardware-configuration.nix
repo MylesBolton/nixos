74077bbd808918b9abad369bea080450696052b9
MylesBolton <git@MylesBolton.com>
2025-08-29 01:19:46 +0100

    slop

--- systems/x86_64-linux/workstation-01/hardware-configuration.nix
+++ systems/x86_64-linux/workstation-01/hardware-configuration.nix
@@ -17,21 +17,13 @@
   ];
   boot.initrd.kernelModules = [
     "xe"
-    "i915"
   ];
   boot.kernelModules = [
     "kvm-intel"
     "sg"
     "xe"
-    "i915"
   ];
-  boot.kernelParams = [
-    "i915.force_probe=1901"
-    "xe.force_probe=1901"
-    "i915.force_probe=e20b"
-    "xe.force_probe=e20b"
-    "i915.enable_guc=2"
-  ];
+  boot.kernelParams = [ ];
   boot.extraModulePackages = [ ];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
