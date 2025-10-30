8e3ab47180c4be7f224a9c872a10b5d2dbbfc5ef
MylesBolton <git@MylesBolton.com>
2025-10-30 18:00:19 +0000

    sec

---
+++ modules/home/roles/sec/default.nix
@@ -0,0 +1,50 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+with lib;
+with lib.custom;
+let
+  cfg = config.roles.sec;
+in
+{
+  options.roles.sec = {
+    enable = mkBoolOpt false "Enable the cybersecurity tools role.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      burpsuite
+      nmap
+      zenmap
+      smap
+      wireshark-cli
+      tcpdump
+      junkie
+      aircrack-ng
+      bettercap
+      hping
+      gotestwaf
+      sqlmap
+      gobuster
+      metasploit
+      armitage
+      thc-hydra
+      hydra-cli
+      mimikatz
+      hashcat
+      ghidra-bin
+      radare2
+      iaito
+      binwalk
+      autopsy
+      sleuthkit
+      cyberchef
+      exploitdb
+    ];
+  };
+}
