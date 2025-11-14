2e4e295c98aa412f6dbf59d1720fba5720cff027
MylesBolton <git@MylesBolton.com>
2025-11-14 16:02:08 +0000

    fix common role

--- modules/home/roles/common/default.nix
+++ modules/home/roles/common/default.nix
@@ -16,48 +16,9 @@ in
     enable = mkBoolOpt false "Enable common role";
   };
 
-  custom = mkIf cfg.enable {
-
-  };
-
   config = mkIf cfg.enable {
-    custom = {
-      cli.shells.fish.enable = true;
-      system = {
-        nix.enable = true;
-        networking.enable = true;
-        locale.enable = true;
-        boot.enable = true;
-      };
-      services = {
-        openssh.enable = true;
-      };
-      user = {
-        name = "user";
-        initialPassword = "1337";
-      };
-    };
-    fonts = {
-      packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-      enableDefaultPackages = true;
-    };
-    environment.systemPackages = with pkgs; [
-      gopass
-      gnupg
-      gpg-tui
-      git
-      unrar
-      unzip
-      zip
-      btop
-      pciutils
-      lsscsi
-      wget
-      curl
-      nmap
-    ];
+    custom.system.nix.enable = true;
+    cli.shells.fish.enable = true;
     styles.stylix.enable = true;
   };
-}
-
 }
