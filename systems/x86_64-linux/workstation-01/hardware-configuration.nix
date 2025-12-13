369c9ed380817655e99c8bac8bac8bcd4d03508a
MylesBolton <git@MylesBolton.com>
2025-12-13 11:27:20 +0000

    gpu stuff

--- systems/x86_64-linux/workstation-01/hardware-configuration.nix
+++ systems/x86_64-linux/workstation-01/hardware-configuration.nix
@@ -12,6 +12,30 @@
 
   environment.variables = {
     GSK_RENDERER = "gl"; # stops wierd driver problems for intel B580 GPU
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+  hardware = {
+    intel-gpu-tools.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # QuickSync (iHD)
+        vpl-gpu-rt # Modern Video Processing Library
+        intel-compute-runtime # OpenCL/Compute
+        libva-vdpau-driver
+        libvdpau-va-gl
+        mesa
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        intel-media-driver
+        intel-vaapi-driver
+        libva-vdpau-driver
+        mesa
+        libvdpau-va-gl
+      ];
+    };
   };
 
   services.xserver.videoDrivers = [ "modesetting" ];
