924950aecaba15aec6c4d51b03f05c682b894da0
mylesbolton <git@mylesbolton.com>
2026-05-21 20:44:14 +0100

    rn

--- flake.nix
+++ flake.nix
@@ -4,12 +4,10 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
-      "https://attic.xuyh0120.win/lantian"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     ];
   };
   inputs = {
